776c965e7735936fab2576d202b2e90014c5568c
maykbrito <maykbrito@gmail.com>
2020-11-03 16:45:15 -0300

    add youtube comments api

---
+++ pages/api/comments.js
@@ -0,0 +1,20 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+const CommentsScraper = require("yt-comment-scraper");
+const ytcomments = new CommentsScraper();
+
+export default async (req, res) => {
+  const videoId = req.query.video;
+
+  try {
+    const data = await ytcomments.scrape_all_youtube_comments(videoId);
+    const doesNotHasReply = data.filter(
+      (comment) => comment.hasReplies == false
+    );
+
+    res.statusCode = 200;
+    return res.json(doesNotHasReply);
+  } catch (error) {
+    res.statusCode = 500;
+    return res.json(error);
+  }
+};
